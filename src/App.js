2b2582e70141e0fdcabf6563566c08bc31673221
LAPTOP-773F5724\9305g <9305good@gmail.com>
2022-05-25 00:04:54 +0900

    #2 일기 리스트 렌더링

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import './App.css';
-import DiaryEditor from './DiaryEditor';
+import { useRef, useState } from "react";
+import "./App.css";
+import DiaryEditor from "./DiaryEditor";
+import DiaryList from "./DiaryList";
 
 function App() {
+  const [data, setData] = useState([]); // 배열이 초기값
+
+  const dataId = useRef(0); // id의 기준이 되는 상수를 useRef사용해서 만들어줌
+
+  const onCreate = (author, content, emotion) => {
+    const created_date = new Date().getTime();
+    const newItem = {
+      author,
+      content,
+      emotion,
+      created_date,
+      id: dataId.current,
+    };
+    dataId.current += 1;
+    setData([newItem, ...data]); // newItem이 가장 위로 올라와야되니까
+  };
+
   return (
     <div className="App">
-      <DiaryEditor/>
+      <DiaryEditor onCreate={onCreate} />
+      <DiaryList diaryList={data} />
     </div>
   );
 }
--- src/DiaryEditor.js
+++ src/DiaryEditor.js
@@ -1,9 +1,13 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
-const DiaryEditor = () => {
+const DiaryEditor = ({ onCreate }) => {
+  const authorInput = useRef();
+  const contentInput = useRef();
+
   const [state, setState] = useState({
     author: "",
     content: "",
+    emotion: 1,
   });
 
   const handleChangeState = (e) => {
@@ -13,39 +17,61 @@ const DiaryEditor = () => {
     });
   };
 
-  //   const [author, setAuthor] = useState("");
-  //   const [content, setContent] = useState("");
+  const handleSubmit = () => {
+    if (state.author.length < 1) {
+      alert("작성자는 최소 1글자 이상 입력해주세요.");
+      authorInput.current.focus();
+      return;
+    }
+    if (state.content.length < 5) {
+      alert("일기 본문은 최소 5글자 이상 입력해주세요.");
+      contentInput.current.focus();
+      return;
+    }
+    onCreate(state.author, state.content, state.emotion);
+    alert("일기 저장 성공");
+    setState({
+      author: "",
+      content: "",
+      emotion: 1,
+    });
+  };
 
   return (
     <div className="DiaryEditor">
       <h2>오늘의 일기</h2>
       <div>
         <input
+          ref={authorInput}
           name="author"
           value={state.author}
           onChange={handleChangeState}
-          //   onChange={(e) => {
-          //     setState({
-          //       ...state,
-          //       author: e.target.value,
-          //       // 순서대로 업데이트 되기 때문에
-          //       // 바뀌어야할 상태가 가장 마지막에 있어야 함
-          //     });
-          //   }}
         />
       </div>
       <div>
         <textarea
+          ref={contentInput}
           name="content"
           value={state.content}
           onChange={handleChangeState}
-          //   onChange={(e) => {
-          //     setState({
-          //       ...state,
-          //       content: e.target.value,
-          //     });
-          //   }}
         />
+      </div>
+      <div>
+        오늘의 감정점수 :
+        <select
+          name="emotion"
+          value={state.emotion}
+          onChange={handleChangeState}
+        >
+          <option value={1}>1</option>
+          <option value={2}>2</option>
+          <option value={3}>3</option>
+          <option value={4}>4</option>
+          <option value={5}>5</option>
+        </select>
+      </div>
+      <div>
+        <button onClick={handleSubmit}>일기 저장하기</button>
       </div>
     </div>
   );
