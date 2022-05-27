70e92f6d70a41dcfbf133168d45aad5cca58f4b0
LAPTOP-773F5724\9305g <9305good@gmail.com>
2022-05-27 23:27:39 +0900

    #4 LifeCycle 알아보기

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import { useRef, useState } from "react";
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
+import Lifecycle from "./Lifecycle";
 
-function App() {
+const App = () => {
   const [data, setData] = useState([]); // 배열이 초기값
 
   const dataId = useRef(0); // id의 기준이 되는 상수를 useRef사용해서 만들어줌
-
   const onCreate = (author, content, emotion) => {
     const created_date = new Date().getTime();
     const newItem = {
@@ -36,10 +36,11 @@ function App() {
 
   return (
     <div className="App">
+      <Lifecycle />
       <DiaryEditor onCreate={onCreate} />
       <DiaryList onEdit={onEdit} onRemove={onRemove} diaryList={data} />
     </div>
   );
-}
+};
 
 export default App;
